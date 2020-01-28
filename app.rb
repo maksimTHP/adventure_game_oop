77798a8af438bf65138daec488ed4e2ef58e9762
Maxime Besse <bessemaxime@gmail.com>
2020-01-28 17:44:13 +0100

    Game 1 terminé

--- app.rb
+++ app.rb
@@ -1,12 +1,41 @@
 # lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
 require 'bundler'
-Bundler.require
+require "pry"
 
-# lignes qui appellent les fichiers lib/user.rb et lib/event.rb
-# comme ça, tu peux faire User.new dans ce fichier d'application. Top.
-require_relative 'lib/user'
-require_relative 'lib/event'
+require_relative 'lib/game'
+require_relative 'lib/player'
 
 
-# Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
-# Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
+
+player1 = Player.new("Josiane")   #On créé une instance de Player appelée player1
+player2 = Player.new("José")      #On créé une instance de Player appelée player2
+
+puts "À ma droite #{player1.name}"  #Affichage du noms des combattants avec instance.name
+puts "À ma gauche #{player2.name}"
+puts player1.show_state              #On affiche l'état des combattants
+puts ""
+puts player2.show_state
+puts "Passons à la phase d'attaque !"
+puts "* Musique de suspense *"
+player1.attacks(player2)          #Utilisation de la méthode attack par l'instance player 1 argument: player2
+player2.attacks(player1)          #Utilisation de la méthode attack par l'instance player 2 argument: player1
+
+while player1.life_points>0 && player2.life_points>0 do     #Boucle while pour générer des bagarres tant que aucun des deux n'est mort.
+  player1.show_state
+  player2.show_state
+  puts "_____NEW ROUND_____"
+  player1.attacks(player2)     #Nouvelle attaque.
+    if player2.life_points <= 0     #Si life_points de player2 tombe à zero, alors il ne peut plus attaquer, puisqu'il est moooooort!
+      break  #Alors on break pour sortir de la boucle while immédiatement. Sinon player2 attaque encore un coup alors qu'il vient de subir une trépanation mortelle!
+  end
+  player2.attacks(player1)
+end
+
+
+
+# if player2.life_points <= 0   #Quand on arrive ici, on sait qu'un de deux est mort, mais on ne sait pas lequel. Sinon on serait pas sortit de la boucle while. Alors si player2 a un nombre de point de vie inférieur ou égal à 0 ca veut dire que c'est lui qui est mort.
+#   then puts "#{player2.name} est mort. #{player1.name} a gagné!" #Alors player2 est mort. Et player 1 a gagné. Bravo!
+# else puts "#{player1.name} est mort. #{player2.name} a gagné!" #Sinon player1 est mort. DAns ce cas c'est tout naturellement que player 2 a gagné. GG gros!
+# end
+
+puts "Fin du game. Bisous."
--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,32 @@
-class Event
+class Player
 
+  attr_accessor :name, :life_points
+
+  def initialize(name) #Ici on décalre notre initialize. C'est cette def qui va servir pour créer notre Player.new on a besoin d'une entrée (le nom du joueur) pour créer une instance qui contiendra le nom et le nombre de point de vie.
+    @name = name
+    @life_points = life_points = 10
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie" #Affche "Nom de l'instance" a "Point de vie de l'instance" points de vie
+  end
+
+  def gets_damage(damage)   #On applique les dommages générés. Point de vie = Point de vie - les dégats
+    @life_points = @life_points - damage
+    if @life_points > 0  #Ici on affiche directement si un des joueurs est mort avec une focntion conditionnelle. Soit il est pas mort et il lui reste des points de vie (then) soit il est mort et on le dit (else)
+      then puts "#{@name} a #{@life_points} points de vie !"
+      else puts "#{@name} n'a plus de point de vie, #{@name} es muerto ! "
+    end
+  end
+
+  def attacks(is_attacked)  #Fonction d'attaque instance.attacks signigie qu'une instance va utiliser cette fonction pour attaquer. Entre parenthèse l'arguement est l'autre joueur forcément. player1.attacks(player2)
+    puts "#{@name} attaque #{is_attacked.name}"
+    current_attack = compute_damage #on lance le dès pour savoir combien de dégat il va prendre et on stocke les dégats dans une variable appelée current_attack
+    puts "Il lui inflige #{current_attack} points de dégat."
+    is_attacked.gets_damage(current_attack) #On utilise la fonction gets_damage en instance on met is_attacked (le player qui sera argument de la fonction attacks) et on lui inflige les dégats stockés dans current_attack
+  end
+
+  def compute_damage  #Roll the dice
+    return rand(1..6)
+  end
 end
